4165281a976aaebef74047599fec1ff88607d479
Andrei Borodin <124201108+AndreiBorod1n@users.noreply.github.com>
2023-02-16 22:25:28 +0500

    Add 3rd exercise

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+Console.Write("Число ");
+Console.Write(number);
+Console.Write(" является четным.");
+}
+else
+{
+Console.Write("Число ");
+Console.Write(number);
+Console.Write(" не является четным.");
+ }
